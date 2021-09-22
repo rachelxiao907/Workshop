c9c396fa31bff4401234809b6136fcd7f27fd6d4
Rachel Xiao <rachelxiao907@gmail.com>
2021-09-22 18:21:06 -0400

    added a heading and more names

--- classwork0.py
+++ classwork0.py
@@ -1,3 +1,8 @@
+# Rachel Xiao
+# SoftDev
+# Classwork0/Random Student Name Using Lists/Created two lists for students in period 1 and 2. Printed a random student name from each period
+# 2021-09-21
+
 import random
 
 #function to get a random name from the list of names
@@ -7,8 +12,8 @@ def randomName(arr):
     #print(index)
     print(arr[index])
 
-pd1 = ["Emma Buller", "Julia Nelson", "Owen Yaggy", "Haotian Gao", "Reng Zheng"]
+pd1 = ["Emma Buller", "Julia Nelson", "Owen Yaggy", "Haotian Gao", "Reng Zheng", "Kevin Cao", "Michelle Lo", "Ivan Lam", "Christopher Liu"]
 randomName(pd1)
 
-pd2 = ["Jesse Xie", "Rachel Xiao", "Yuqing Wu", "Liesel Wong", "Josephine Lee", "Yaying Liang", "Justin Zou"]
+pd2 = ["Jesse Xie", "Rachel Xiao", "Yuqing Wu", "Liesel Wong", "Josephine Lee", "Yaying Liang", "Justin Zou", "Patrick Ging", "Raymond Yueng", "Michael Borczuk"]
 randomName(pd2)
