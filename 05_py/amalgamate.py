fa8b7363354d8845bb552c818ba5241c56200a90
Rachel Xiao <rachelxiao907@gmail.com>
2021-09-26 15:01:39 -0400

    finished amalgamating

--- 05_py/amalgamate.py
+++ 05_py/amalgamate.py
@@ -1,25 +1,68 @@
-# Rachel Xiao
+# Rachel Xiao, Eliza Knapp, Thomas Yu
 # SoftDev
 # K05 -- Amalgamate/Combining Code/Found the best way to print a random student name from each period between me, Eliza, and Thomas.
-# 2021-09-24
+# 2021-09-26
 
-# Summary:
-# Discoveries:
-# Questions:
-# Comments:
+'''
+Summary:
+    After looking at all three of our files, we decided that it would be easiest
+    to read the names off of two separate text files, one for period 1 and the
+    other for period 2. The generate_name function takes in each of those files
+    and tries to open it (uses try and except courtesy of Thomas' group). It adds the
+    contents to a list, shuffles it, and prints the first index. If opening
+    the files or looking at their content is not possible, it prints the according
+    error message.
+
+Discoveries:
+    - We learned how to use try: and except: in python (none of us took Annual CS).
+    - I learned how to read a file and store its information in a list.
+
+Questions:
+    - Should we print or return the names inside of the function?
+    - How might we change the code to make it run for infinite periods of students?
+    - Is the runtime of shuffle significantly longer than random.randint for longer lists?
+
+Comments:
+    - Although it is faster to generate a random integer and take that index of the
+    array than shuffling, we ended up going with random.shuffle(array) because
+    it looks prettier in the code and the lists aren't too long.
+
+'''
 
 import random
 
-#function to get a random name from the list of names
-def randomName(arr):
-    length = len(arr)
-    index = random.randint(0, length-1) #inclusive
-    #print(index)
-    print(arr[index])
+def generate_name(pd1_file, pd2_file):
+    pd1 = []
+    pd2 = []
 
-pd1 = ["Emma Buller", "Julia Nelson", "Owen Yaggy", "Haotian Gao", "Reng Zheng", "Kevin Cao", "Michelle Lo", "Ivan Lam", "Christopher Liu"]
-randomName(pd1)
+    #pd1
+    try:
+        pd1_names = open(pd1_file)
+        for line in pd1_names:
+            pd1.append(line.strip())
+        pd1_names.close()
 
-pd2 = ["Jesse Xie", "Rachel Xiao", "Yuqing Wu", "Liesel Wong", "Josephine Lee", "Yaying Liang", "Justin Zou", "Patrick Ging", "Raymond Yueng", "Michael Borczuk"]
-randomName(pd2)
+        try:
+            random.shuffle(pd1)
+            print("pd1: " + pd1[0])
+        except:
+            print("Pd1 text file is empty")
+    except:
+        print("The pd1 file doesn't exist")
 
+    #pd2
+    try:
+        pd2_names = open(pd2_file)
+        for line in pd2_names:
+            pd2.append(line.strip()) #strip removes the \n at the end of each line
+        pd2_names.close()
+
+        try:
+            random.shuffle(pd2)
+            print("pd2: " + pd2[0])
+        except:
+            print("Pd2 text file is empty")
+    except:
+        print("The pd2 file doesn't exist")
+
+generate_name("pd1.txt", "pd2.txt")
