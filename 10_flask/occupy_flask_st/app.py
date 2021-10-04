d0d933d5fcc4809a5cb5f8364bfe958ce365380f
Rachel Xiao <rachelxiao907@gmail.com>
2021-10-04 18:13:19 -0400

    files for occupation-chooser

--- 10_flask/occupy_flask_st/app.py
+++ 10_flask/occupy_flask_st/app.py
@@ -7,14 +7,9 @@ from flask import Flask
 app = Flask(__name__) #create instance of class Flask
 
 @app.route("/")       #assign fxn to route
-def hello_world():
-    print("the __name__ of this module is... ")
-    print(__name__)
-    return "No hablo queso!"
+def print_occupations():
+    return 
 
 if __name__ == "__main__":  # true if this file NOT imported
     app.debug = True        # enable auto-reload upon code change
     app.run()
-
-# The app only runs if the file is not imported.
-# Except the checking of file importation, the observations from v3 holds true for this app.
