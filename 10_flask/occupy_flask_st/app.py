0eab8e4753bf4a87a5b4b323eef4a0f3c249ac25
Rachel Xiao <rachelxiao907@gmail.com>
2021-10-05 17:36:12 -0400

    k10

--- 10_flask/occupy_flask_st/app.py
+++ 10_flask/occupy_flask_st/app.py
@@ -4,11 +4,19 @@
 # 2021-10-04
 
 from flask import Flask
+import K06
 app = Flask(__name__) #create instance of class Flask
 
 @app.route("/")       #assign fxn to route
-def print_occupations():
-    return 
+def hello_world():
+    print("the __name__ of this module is... ")
+    print(__name__)
+    
+    heading = '''Team Polar: Yuqing Wu, Jesse Xie, Rachel Xiao <br/>
+    SoftDev <br/>
+    K10 -- Putting Little Pieces Together<br/>
+    2021-10-04 <br/><br/>'''
+    return heading + K06.main() + "<br/>\n<br/>" + str(K06.job_classes)
 
 if __name__ == "__main__":  # true if this file NOT imported
     app.debug = True        # enable auto-reload upon code change
