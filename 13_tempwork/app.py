aaf5cc8f159da1d38c874016aebbecaf293ed789
Rachel Xiao <rachelxiao907@gmail.com>
2021-10-10 14:34:38 -0400

    finished flask app and printed occupation chooser on webpage

--- 13_tempwork/app.py
+++ 13_tempwork/app.py
@@ -2,3 +2,36 @@
 # SoftDev
 # K13 -- Template for Success
 # 2021-10-08
+
+from flask import Flask, render_template
+import csv
+import random
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "No hablo queso!"
+
+occupations = {}
+
+with open('data/occupations.csv') as csv_file: #give access to the file by opening it
+    reader = csv.reader(csv_file, delimiter=',') #delimeter puts values in a list for each row as it splits by commas
+    for row in reader:
+        if row[0] != "Job Class": #don't store the heading or total
+            occupations[row[0]] = float(row[1]) #put the data in a dictionary
+            
+def make_choice():
+    jobs = list(occupations.keys()) # convert the keys into a list
+    jobs.pop(len(jobs)-1)
+    percentages = list(occupations.values())
+    percentages.pop(len(percentages)-1)
+    choice = random.choices(jobs, weights = percentages, k=1)
+    return choice[0]
+
+@app.route("/occupyflaskst")
+def test_tmplt():
+    return render_template('tablified.html', foo = "Occupation Chooser", dict = occupations, selected_occupation = make_choice())
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
