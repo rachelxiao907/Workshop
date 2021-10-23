9648fe5648ac6c3495db4780fb3a200ec9919dac
Rachel Xiao <rachelxiao907@gmail.com>
2021-10-22 23:48:33 -0400

    old

---
+++ 16_csv2d/db_builder.py
@@ -0,0 +1,29 @@
+# Team Marshmallow: Rachel Xiao (Mooana), Liesel Wong (King Hagrid), Yoonah Chang (Yelena)
+# SoftDev
+# K16 -- All About Database
+# 2021-10-22
+
+import sqlite3   #enable control of an sqlite database
+import csv       #facilitate CSV I/O
+
+
+DB_FILE="discobandit.db"
+
+db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+c = db.cursor()               #facilitate db ops -- you will use cursor to trigger db events
+
+#==========================================================
+
+
+# < < < INSERT YOUR TEAM'S POPULATE-THE-DB CODE HERE > > >
+
+
+students_file = csv.DictReader(open("students.csv")) # values in the first row are the keys of the dict
+courses_file = csv.DictReader(open("courses.csv"))
+# for row in students_file: # check how the dictionary reader looks like
+#    print(row)
+
+#==========================================================
+
+db.commit() #save changes
+db.close()  #close database
