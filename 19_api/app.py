a3e173102506d9feb860c148c5fe3879bfa19ed4
Rachel Xiao <rachelxiao907@gmail.com>
2021-11-23 17:05:06 -0500

    Flask app displays image and explanation

--- 19_api/app.py
+++ 19_api/app.py
@@ -1,9 +1,23 @@
+# Wandering Wolverine: Rachel Xiao, Wen Hao Dong
+# SoftDev pd2
+# K19 -- A RESTful Journey Skyward
+# 2021-11-23
+# time spent: 1 hour
+
+from flask import Flask, render_template
 from urllib import request
 import json
 
-r = request.urlopen("https://api.nasa.gov/planetary/apod?api_key=Bh5EFULUqcQmnVqskzAFlyrq3d4Ubw6vcIzsIq30")
+app = Flask(__name__)
 
-d = json.loads(r.read())
-print(d)
+@app.route("/")
+def disp_page():
+    page = request.urlopen("https://api.nasa.gov/planetary/apod?api_key=Bh5EFULUqcQmnVqskzAFlyrq3d4Ubw6vcIzsIq30")
+    print(page)
+    url_dict = json.loads(page.read())
+    print(url_dict['url'])
+    return render_template('main.html', pic=url_dict['url'], explanation=url_dict['explanation'])
 
-print(d["url"])
+if __name__ == "__main__":  # true if this file NOT imported
+    app.debug = True        # enable auto-reload upon code change
+    app.run()
