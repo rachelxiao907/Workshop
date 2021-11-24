9a9b636f203ceb3cf6497bcadc0df4892297ab58
Rachel Xiao <rachelxiao907@gmail.com>
2021-11-23 20:32:30 -0500

    read key from text file

--- 19_api/app.py
+++ 19_api/app.py
@@ -2,7 +2,7 @@
 # SoftDev pd2
 # K19 -- A RESTful Journey Skyward
 # 2021-11-23
-# time spent: 1 hour
+# time spent: 45 minutes
 
 from flask import Flask, render_template
 from urllib import request
@@ -10,12 +10,17 @@ import json
 
 app = Flask(__name__)
 
+KEY = ""
+with open("key_nasa.txt", "r") as key_file:
+    KEY = key_file.read()
+
 @app.route("/")
 def disp_page():
-    page = request.urlopen("https://api.nasa.gov/planetary/apod?api_key=Bh5EFULUqcQmnVqskzAFlyrq3d4Ubw6vcIzsIq30")
-    print(page)
-    url_dict = json.loads(page.read())
-    print(url_dict['url'])
+    page = request.urlopen(f"https://api.nasa.gov/planetary/apod?api_key={KEY}") #f string to add key to the url
+    #print(page)
+    url_dict = json.loads(page.read()) #turns the reading of the link which uses JSON format into a dictionary
+    #print(url_dict)
+    #print(url_dict['url'])
     return render_template('main.html', pic=url_dict['url'], explanation=url_dict['explanation'])
 
 if __name__ == "__main__":  # true if this file NOT imported
