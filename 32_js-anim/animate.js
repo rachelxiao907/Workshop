20cb4d8917964566358c3150be0444a44b88e83d
Rachel Xiao <rachelxiao907@gmail.com>
2022-02-17 09:25:25 -0500

    classwork

---
+++ 32_js-anim/animate.js
@@ -0,0 +1,117 @@
+// Team Odin :: Yuqing Wu, Rachel Xiao
+// SoftDev pd2
+// K32 -- More Moving Parts
+// 2022-02-17r
+
+// model for HTML5 canvas-based animation
+
+// SKEELTON
+
+
+//access canvas and buttons via DOM
+var c = document.getElementById('playground');
+var dotButton = document.getElementById('buttonCircle');
+var dvdButton = document.getElementById('buttonDVD');
+var stopButton = document.getElementById('buttonStop');
+
+//prepare to interact with canvas in 2D
+var ctx = c.getContext('2d');
+
+//set fill color to team color
+ctx.fillStyle = "skyblue";
+
+var requestID;  //init global var for use with animation frames
+
+
+//var clear = function(e) {
+var clear = (e) => {
+  console.log("clear invoked...")
+  ctx.clearRect(0, 0, c.clientWidth , c.clientHeight);
+};
+
+
+var radius = 0;
+var growing = true;
+
+
+//var drawDot = function() {
+var drawDot = () => {
+  /*
+    ...to
+    Wipe the canvas,
+    Repaint the circle,
+    ...and somewhere (where/when is the right time?)
+    Update requestID to propagate the animation.
+    You will need
+    window.cancelAnimationFrame()
+    window.requestAnimationFrame()
+   */
+  clear(); //wipe canvas
+  stopIt(); //to propagate your animations, you must pop off existing frames from the stack
+            //cancelAnimationFrame first to make sure there is one animation frame running
+  console.log("drawDot invoked...");
+  //start drawing circle from the center
+  var mouseX = c.clientWidth / 2;
+  var mouseY = c.clientHeight / 2;
+  console.log("mouseClick registered at ", mouseX, mouseY);
+
+  //repaint circle
+  ctx.strokeStyle = "black";
+  ctx.beginPath();
+  ctx.arc(mouseX, mouseY, radius, 0, 2*Math.PI);
+  ctx.fill();
+  ctx.stroke();
+
+  //circle shouldn't be drawn outside of the bounds of the canvas
+  //circle will start getting smaller when it hits the edges
+  if (radius == Math.min(c.clientWidth, c.clientHeight) / 2) {
+    growing = false;
+  }
+  //circle will get bigger if it is at its smallest
+  if (radius == 0) {
+    growing = true;
+  }
+
+  if (growing) {
+    radius++;
+  } else {
+    radius--;
+  }
+
+  requestID = window.requestAnimationFrame(drawDot); //and provide a callback to continue animating
+};
+
+
+var drawDVD = () => {
+  clear();
+  stopIt();
+  console.log("drawDVD invoked...");
+  var width = 100;
+  var height = 50;
+  var x = Math.floor(Math.random() * (c.clientWidth - width));
+  var y = Math.floor(Math.random() * (c.clientHeight - height));
+  console.log("mouseClick registered at ", x, y);
+
+  var dvdImage = new Image();
+  dvdImage.src = "logo_dvd.jpg";
+  ctx.drawImage(dvdImage, x, y, width, height);
+}
+
+
+//var stopIt = function() {
+var stopIt = () => {
+  /*
+    ...to
+    Stop the animation
+    You will need
+    window.cancelAnimationFrame()
+  */
+  console.log("stopIt invoked...")
+  console.log( requestID );
+  window.cancelAnimationFrame(requestID);
+};
+
+
+dotButton.addEventListener( "click", drawDot );
+dvdButton.addEventListener( "click",  drawDVD );
+stopButton.addEventListener( "click",  stopIt );
